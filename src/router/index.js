486dda1b2cbe38cb74148c4f0c48d3835b259f94
rovast <rovast@163.com>
2022-04-19 15:02:41 +0800

    ✨ 完成客户端列表的获取

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -8,16 +7,28 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import('@/views/HomeView')
+  },
+  {
+    path: '/clients',
+    name: 'clients',
+    component: () => import('@/views/clients')
+  },
+  {
+    path: '/servers',
+    name: 'servers',
+    component: () => import('@/views/clients')
+  },
+  {
+    path: '/resources',
+    name: 'resources',
+    component: () => import('@/views/clients')
+  },
+  {
+    path: '/relay',
+    name: 'relay',
+    component: () => import('@/views/clients')
   }
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
 ]
 
 const router = new VueRouter({
