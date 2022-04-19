6f49473e658149ed62361a871d667821cfe3141c
rovast <rovast@163.com>
2022-04-19 13:16:44 +0800

    ✨ finish layout
    Signed-off-by: rovast <rovast@163.com>

--- src/router/index.js
+++ src/router/index.js
@@ -9,15 +9,15 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = new VueRouter({
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,8 @@
 <template>
-  <hello-world />
+  <div class="text-center ">
+    <h1 class="text-h1 font-weight-thin mt-10">Welcome</h1>
+    <p class="text-body-1 mt-10">
+      🛡ZTALAB🛡 is a research and open source organization for zero trust architecture. We will gradually open source the projects we have incubated and ongoing projects to make our contribution to the zero-trust ecosystem. If you are also interested, or have any suggestions, please let us know and let us know. Let's work together to promote the construction of a zero-trust ecosystem.
+    </p>
+  </div>
 </template>
-
-<script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
-</script>
