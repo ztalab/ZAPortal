ef3e8e3a06e56441c996bfd15d0567840e9b699a
rovast <rovast@163.com>
2022-04-19 09:51:25 +0800

    📦 vue add vuetify
    Signed-off-by: rovast <rovast@163.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,12 @@ module.exports = {
       env: {
         jest: true
       }
+    },
+    {
+      files: ['src/views/**/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 0,
+      },
     }
   ]
 }
